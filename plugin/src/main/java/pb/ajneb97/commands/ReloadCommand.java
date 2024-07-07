3c4c91e4913564c3e5964a67656f3db168952d6e
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2024-07-06 20:08:43 -0400

    chore: Recover some code from old command class.

---
+++ plugin/src/main/java/pb/ajneb97/commands/ReloadCommand.java
@@ -0,0 +1,41 @@
+package pb.ajneb97.commands;
+
+import dev.dejvokep.boostedyaml.YamlDocument;
+import dev.triumphteam.cmd.core.annotation.SubCommand;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.entity.Player;
+import pb.ajneb97.PaintballBattle;
+
+import java.io.IOException;
+
+public class ReloadCommand extends MainCommand {
+
+    private PaintballBattle plugin;
+    private YamlDocument messages;
+
+    public ReloadCommand(PaintballBattle plugin) {
+        super(plugin);
+        this.plugin = plugin;
+        this.messages = plugin.getMessagesDocument();
+    }
+
+    @SubCommand(value = "reload")
+    public void command(Player player) {
+        if (player.isOp() || player.hasPermission("paintball.admin")) {
+            try {
+                plugin.getShopDocument().reload();
+                plugin.getConfigDocument().reload();
+                plugin.getMessagesDocument().reload();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            plugin.recargarCarteles();
+            //plugin.recargarScoreboard();
+            //TODO
+            //plugin.recargarHologramas();
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', messages.getString("configReloaded")));
+        } else {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', messages.getString("noPermissions")));
+        }
+    }
+}
