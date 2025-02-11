6162553827de95dea08b84bbbfd5f514ae945f93
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2025-02-11 18:57:12 -0300

    chore(Inventories): Add interface class to create inventories.

---
+++ plugin/src/main/java/pb/ajneb97/inventories/util/BaseMenu.java
@@ -0,0 +1,45 @@
+package pb.ajneb97.inventories.util;
+
+import dev.triumphteam.gui.builder.item.ItemBuilder;
+import dev.triumphteam.gui.guis.Gui;
+import dev.triumphteam.gui.guis.GuiItem;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public interface BaseMenu {
+
+    /**
+     * @param player Player to open the menu to.
+     */
+    default void open(Player player) {
+
+    }
+
+
+    default void open(Player player, Object... objects) {
+
+    }
+
+    default void addMenuItem(Gui menu, int slot, Material material, Component name, List<Component> lore, Consumer<InventoryClickEvent> clickAction) {
+        GuiItem item = ItemBuilder.from(material)
+                .name(name)
+                .lore(lore)
+                .lore(lore)
+                .asGuiItem();
+
+        item.setAction(event -> {
+            ItemStack stack = event.getCurrentItem();
+            if (stack != null && stack.isSimilar(item.getItemStack())) {
+                clickAction.accept(event);
+            }
+        });
+
+        menu.setItem(slot, item);
+    }
+}
